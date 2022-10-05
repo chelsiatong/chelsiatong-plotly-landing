c4049d2df857223332f628b0cbc5d9a0a1e44591
chelsia tong <chelsiatong@gmail.com>
2022-10-05 16:55:34 -0400

    adding responsive design for desktop

--- src/components/DarkMode.js
+++ src/components/DarkMode.js
@@ -1,28 +1,33 @@
-import { useColorMode, Button, IconButton } from '@chakra-ui/react'
+import { useColorMode, Button } from '@chakra-ui/react'
 import { MoonIcon, SunIcon, ChevronDownIcon } from '@chakra-ui/icons'
 import { Menu, MenuButton, MenuList, MenuItem } from '@chakra-ui/react'
 import { Icon } from '@chakra-ui/icons'
 import { System } from '../assets/logo/system-icon.svg'
 
+
 const DarkMode = () => {
-    const { colorMode, toggleColorMode } = useColorMode()
+    const { colorMode, toggleColorMode } = useColorMode();
+    // const [isActive, setIsActive] = useState(false);
+
+    // const handleClick = () => {
+    //     setIsActive(!isActive);
+    // };
+
+
     return (
-        // <Button aria-label="Toggle Dark Switch"
-        //     icon={colorMode === 'dark' ? <SunIcon /> : <MoonIcon />}
-        //     onClick={toggleColorMode} >
-        //     click me
-        // </Button>
 
         <Menu aria-label="Toggle Dark Switch" >
-            <MenuButton as={Button} mt='0.35rem' rightIcon={<ChevronDownIcon />} >
+            <MenuButton aria-label="Toggle Arrow" as={Button} fontWeight='400' color='#7E8489' bgColor='rgba(126, 132, 137, 0.1);' fontSize='xs' mt='0.35rem' >
                 <SunIcon />  Light
+                <ChevronDownIcon ml='0.5rem' />
             </MenuButton>
-            <MenuList icon={colorMode === 'dark' ? <SunIcon /> : <MoonIcon />} >
-                <MenuItem onClick={toggleColorMode} > <SunIcon mr='1rem' />Light</MenuItem>
-                <MenuItem onClick={toggleColorMode}> <MoonIcon mr='1rem' />Dark</MenuItem>
-                <MenuItem> <Icon as={System} mr='1rem' /> System</MenuItem>
+            <MenuList icon={colorMode === 'dark'} >
+                <MenuItem onClick={toggleColorMode} fontSize='xs'><SunIcon mr='1rem' />Light</MenuItem>
+                <MenuItem onClick={toggleColorMode} fontSize='xs'><MoonIcon mr='1rem' />Dark</MenuItem>
+                <MenuItem fontSize='xs'> <Icon as={System} mr='1rem' /> System</MenuItem>
             </MenuList>
         </Menu>
+
     )
 }
 
