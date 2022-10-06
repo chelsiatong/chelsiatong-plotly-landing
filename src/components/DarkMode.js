3cd125ef6d1c1271bd4d299572f6c02fb544a6aa
chelsia tong <chelsiatong@gmail.com>
2022-10-06 17:38:13 -0400

    fixing light and dark mode, adding functions

--- src/components/DarkMode.js
+++ src/components/DarkMode.js
@@ -1,33 +1,42 @@
-import { useColorMode, Button } from '@chakra-ui/react'
-import { MoonIcon, SunIcon, ChevronDownIcon } from '@chakra-ui/icons'
+import { useColorMode, Button, Box } from '@chakra-ui/react'
+import { MoonIcon, SunIcon, ChevronDownIcon, ChevronUpIcon } from '@chakra-ui/icons'
 import { Menu, MenuButton, MenuList, MenuItem } from '@chakra-ui/react'
-import { Icon } from '@chakra-ui/icons'
-import { System } from '../assets/logo/system-icon.svg'
-
+import { Icon } from '@chakra-ui/react'
+import { GoSettings } from 'react-icons/go'
 
 const DarkMode = () => {
     const { colorMode, toggleColorMode } = useColorMode();
-    // const [isActive, setIsActive] = useState(false);
 
-    // const handleClick = () => {
-    //     setIsActive(!isActive);
-    // };
+    const handleDark = () => {
+        if (colorMode !== 'dark') {
+            toggleColorMode()
+        }
+    }
 
+    const handleLight = () => {
+        if (colorMode === 'dark') {
+            toggleColorMode()
+        }
+    }
 
     return (
 
         <Menu aria-label="Toggle Dark Switch" >
-            <MenuButton aria-label="Toggle Arrow" as={Button} fontWeight='400' color='#7E8489' bgColor='rgba(126, 132, 137, 0.1);' fontSize='xs' mt='0.35rem' >
-                <SunIcon />  Light
-                <ChevronDownIcon ml='0.5rem' />
-            </MenuButton>
-            <MenuList icon={colorMode === 'dark'} >
-                <MenuItem onClick={toggleColorMode} fontSize='xs'><SunIcon mr='1rem' />Light</MenuItem>
-                <MenuItem onClick={toggleColorMode} fontSize='xs'><MoonIcon mr='1rem' />Dark</MenuItem>
-                <MenuItem fontSize='xs'> <Icon as={System} mr='1rem' /> System</MenuItem>
-            </MenuList>
+            {({ isOpen }) => (
+                <Box>
+                    <MenuButton isActive={isOpen} as={Button} fontWeight='400' color='#7E8489' bgColor='rgba(126, 132, 137, 0.1);' fontSize='xs' mt='0.35rem'  >
+                        {colorMode === 'dark' ? <MoonIcon /> : <SunIcon />}
+                        {colorMode === 'dark' ? 'Dark' : 'Light'}
+                        {isOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
+                    </MenuButton>
+                    <MenuList icon={colorMode === 'dark'} >
+                        <MenuItem onClick={handleLight} fontSize='xs'><SunIcon mr='1rem' />Light</MenuItem>
+                        <MenuItem onClick={handleDark} fontSize='xs'><MoonIcon mr='1rem' />Dark</MenuItem>
+                        <MenuItem fontSize='xs'> <Icon as={GoSettings} mr='1rem' /> System</MenuItem>
+                    </MenuList>
+                </Box>
+            )}
         </Menu>
-
     )
 }
 
