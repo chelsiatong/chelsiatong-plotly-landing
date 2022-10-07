213b8d3e8438b44255c91d123f515b4b7df9c140
chelsia tong <chelsiatong@gmail.com>
2022-10-06 20:10:11 -0400

    fixed menu bar layering

--- src/components/DarkMode.js
+++ src/components/DarkMode.js
@@ -9,7 +9,7 @@ const DarkMode = (props) => {
 
     return (
 
-        <Menu aria-label="Toggle Dark Switch" >
+        <Menu aria-label="Toggle Dark Switch">
             {({ isOpen }) => (
                 <Box>
                     <MenuButton isActive={isOpen} as={Button} fontWeight='400' color='#7E8489' bgColor='rgba(126, 132, 137, 0.1);' fontSize='xs' mt='0.35rem'  >
@@ -17,10 +17,10 @@ const DarkMode = (props) => {
                         {colorMode === 'dark' ? 'Dark' : 'Light'}
                         {isOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
                     </MenuButton>
-                    <MenuList icon={colorMode === 'dark'} >
+                    <MenuList zIndex={{ xl: '9999' }} icon={colorMode === 'dark'} >
                         <MenuItem onClick={props.handleLight} fontSize='xs'><SunIcon mr='1rem' />Light</MenuItem>
                         <MenuItem onClick={props.handleDark} fontSize='xs'><MoonIcon mr='1rem' />Dark</MenuItem>
-                        <MenuItem fontSize='xs'> <Icon as={GoSettings} mr='1rem' /> System</MenuItem>
+                        <MenuItem onClick={props.defaultMode} fontSize='xs'> <Icon as={GoSettings} mr='1rem' /> System</MenuItem>
                     </MenuList>
                 </Box>
             )}
